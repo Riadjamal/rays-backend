78a5d346591295a0ebe0eebc076071a7565907ef
Ahmed Bilal Khan <admin@example.com>
2026-05-21 11:47:29 -0700

    Fix payload size limit for base64 file uploads

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.use(cors({
 
 // Handle preflight requests for all routes
 app.options('*', cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev')); // Logging
 
 // Rate limiting
